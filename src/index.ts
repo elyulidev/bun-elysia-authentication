a6a654993f02085545370e9f3229c8beeb25ac04
elyulidev <elyuli.dev@gmail.com>
2026-02-28 16:04:13 +0200

    refactor: streamline rate limit skip logic and enhance error handling

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,7 @@ const server = new Elysia({
 		rateLimit({
 			duration: 60 * 1000, // janela de 1 minuto
 			max: 10, // máximo de 10 requisições por IP
-			skip: (req) => {
-				console.log("URL>>>", req.url);
-				return !req.url.includes("/api/auth");
-			},
+			skip: (req) => !req.url.includes("/api/auth"), // Não aplicar limite nas rotas de autenticação
 		}),
 	) // Limite de 10 reqs por minuto, pero sin limitar las rutas de auth
 	.get("/", () => ({ message: "Hello, Elysia!" }))
@@ -30,18 +27,22 @@ const server = new Elysia({
 				.use(usersRoute) // /api/users/me (requer auth)
 				.use(adminRoute), // /api/admin/* (requer roles)
 	)
-	.onError(({ code, error }) => {
+	.onError(({ code, error, status }) => {
+		console.log("Code>>", code);
+		console.log("Error>>>", (error as Error)?.message);
 		switch (code) {
-			case 400:
-				return { status: 400, message: "Bad Request" };
-			case 401:
-				return { status: 401, message: "Unauthorized" };
-			case 404:
-				return { status: 404, message: "Not Found" };
+			case "PARSE":
+				return status(400, { message: error.message });
+			case "INVALID_COOKIE_SIGNATURE":
+				return status(400, { message: error.message });
 			case "VALIDATION":
-				return { status: 400, message: error.detail(error.message) };
+				return status(400, { message: error.message });
+			case "NOT_FOUND":
+				return status(404, { message: error.message });
 			default:
-				return { status: 500, message: "Internal Server Error" };
+				return status(500, {
+					message: (error as Error)?.message || "Internal Server Error",
+				});
 		}
 	})
 	.listen(3000);
