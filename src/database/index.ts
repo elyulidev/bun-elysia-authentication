34c76c81bdab28ac6c6dc7fab65a28ad2395802b
elyulidev <elyuli.dev@gmail.com>
2026-03-11 12:35:50 +0200

    fix: remove debug log for database connection test

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,7 +7,6 @@ const client = postgres(process.env.DATABASE_URL!);
 
 const connectionTest = async () => {
 	try {
-		console.log("TESTING CONNECTION", Bun.env.DATABASE_URL);
 		await client`SELECT 1`;
 	} catch (error) {
 		console.error("Error testing database connection:", error);
